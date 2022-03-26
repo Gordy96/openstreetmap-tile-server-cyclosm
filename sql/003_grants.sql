d53069af6f54ceb9c94fd65661e19f8cafc41a61
hpetrovych <gordy.cp96@gmail.com>
2022-03-26 22:43:06 +0200

    sql scripts

---
+++ sql/003_grants.sql
@@ -0,0 +1,10 @@
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA gis TO renderer;
+GRANT ALL PRIVILEGES ON cyclosm_ways TO renderer;
+GRANT ALL PRIVILEGES ON cyclosm_amenities_point TO renderer;
+GRANT ALL PRIVILEGES ON cyclosm_amenities_poly TO renderer;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA gis TO postgres;
+GRANT ALL PRIVILEGES ON cyclosm_ways TO postgres;
+GRANT ALL PRIVILEGES ON cyclosm_amenities_point TO postgres;
+GRANT ALL PRIVILEGES ON cyclosm_amenities_poly TO postgres;  
